fcc5fd568ea017c4da53aafdd735e0e82b6f45f3
Enrico Rossini <enrico.rossini.uk@live.com>
2022-09-23 13:26:20 +0100

    First upload

---
+++ BlazorDynamicForm/BlazorDynamicForm/Shared/Form.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace BlazorDynamicForm.Shared
+{
+    public class Form
+    {
+        public List<Element> Elements { get; set; } = new List<Element>();
+    }
+}
---
+++ BlazorDynamicForm/BlazorDynamicForm/Shared/RadioButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace BlazorDynamicForm.Shared
+{
+    public class RadioButton : Element
+    {
+        public override string ElementType { get => "RadioButton"; }
+
+        public Dictionary<string, string> Options { get; set; }
+
+    }
+}
